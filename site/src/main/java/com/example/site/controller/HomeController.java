5e83cbb5997baa7f793c1f262c16094e3645db46
Alisher <alysher07@mail.ru>
2022-04-25 20:06:45 +0600

    SR-24: чат общий

--- site/src/main/java/com/example/site/controller/HomeController.java
+++ site/src/main/java/com/example/site/controller/HomeController.java
@@ -4,7 +4,12 @@ import com.example.site.domain.Users;
 import com.example.site.service.ExchangeService;
 import com.example.site.service.OfferService;
 import com.example.site.service.UserService;
+import com.example.site.util.ChatMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -13,12 +18,43 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
 
     @Autowired
     private UserService userService;
+
+    @RequestMapping("/chat")
+    public String index(HttpServletRequest request, Model model) {
+
+        String username = getUserData().getFullName();
+
+        if (username == null || username.isEmpty()) {
+            return "redirect:/login";
+        }
+        model.addAttribute("username", username);
+
+        return "chat";
+    }
+
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/publicChatRoom")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/publicChatRoom")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
 
     @Autowired
     private ExchangeService exchangeService;
